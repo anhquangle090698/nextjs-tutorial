d3188496032d4501a554ad0ea5dd287e436a0d65
anhquang <leanhquang.ktpm@gmail.com>
2022-09-19 17:40:37 +0700

    setup prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
+          Anh Quang
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
